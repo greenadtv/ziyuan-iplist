b2d56da950308640f8f0d8b611fd73e967910a3d
greenadtv <greenadtv@outlook.com>
2025-09-27 11:56:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ export default {
             // 获取 Durable Object 实例
             const id = env.IPListDO.idFromName('singleton');
             const obj = env.IPListDO.get(id);
-            return obj.fetch(request);
+
+            // 卑微的我必须等待 DO 的 fetch 返回 Response
+            const response = await obj.fetch(request);
+            return response;
+
         } catch (err) {
             console.error('Error in main Worker fetch:', err);
             return new Response('Internal Error', { status: 500 });
